637e030118529c4753bc51b51b4e3befff2f89bd
bhaveshb1012 <BhaveshbhaiRameshbhai.Bhadiyadra@metropolia.fi>
2024-09-06 12:21:15 +0300

    okay at 12:21

--- bhavesh py.py
+++ bhavesh py.py
@@ -2,3 +2,8 @@ print("Hello Bhavesh")
 
 name = input("Name? ")
 print(f"Hello, {name}! Nice to meet you!")
+
+import math
+radius = float(input("Enter the radius of the circle: "))
+area = math.pi * radius ** 2
+print(f"The area of the circle with radius {radius} is {area:.2f}")
