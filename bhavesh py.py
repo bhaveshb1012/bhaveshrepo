c8f56a47ea543e11ffb6c85caf6606f90ddf4288
bhaveshb1012 <BhaveshbhaiRameshbhai.Bhadiyadra@metropolia.fi>
2024-09-06 12:17:53 +0300

    okay at 12:17

--- bhavesh py.py
+++ bhavesh py.py
@@ -1,4 +1,6 @@
-import math
-radius = float(input("Enter the radius of the circle: "))
-area = math.pi * radius ** 2
-print(f"The area of the circle with radius {radius} is {area:.2f}")
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+perimeter = 2 * (length + width)
+area = length * width
+print(f"The perimeter of the rectangle is {perimeter:.2f}")
+print(f"The area of the rectangle is {area:.2f}")
