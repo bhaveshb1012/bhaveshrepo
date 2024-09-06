7ac0cecac1ae08a511a3de7ec19be22e47e89743
bhaveshb1012 <BhaveshbhaiRameshbhai.Bhadiyadra@metropolia.fi>
2024-09-06 12:31:59 +0300

    okay at 12:31

--- bhavesh py.py
+++ bhavesh py.py
@@ -15,3 +15,14 @@ area = length * width
 print(f"The perimeter of the rectangle is {perimeter:.2f}")
 print(f"The area of the rectangle is {area:.2f}")
 
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+num3 = int(input("Enter the third integer: "))
+
+sum_of_numbers = num1 + num2 + num3
+product_of_numbers = num1 * num2 * num3
+average_of_numbers = sum_of_numbers / 3
+
+print(f"The sum of the numbers is: {sum_of_numbers}")
+print(f"The product of the numbers is: {product_of_numbers}")
+print(f"The average of the numbers is: {average_of_numbers:.2f}")
