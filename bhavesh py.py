44c37b102b862257cbf8add058b41de11dd957fb
bhaveshb1012 <BhaveshbhaiRameshbhai.Bhadiyadra@metropolia.fi>
2024-09-06 12:18:50 +0300

    okay at 12:17

--- bhavesh py.py
+++ bhavesh py.py
@@ -1,6 +1,2 @@
-length = float(input("Enter the length of the rectangle: "))
-width = float(input("Enter the width of the rectangle: "))
-perimeter = 2 * (length + width)
-area = length * width
-print(f"The perimeter of the rectangle is {perimeter:.2f}")
-print(f"The area of the rectangle is {area:.2f}")
+print("Hello Bhavesh")
+
