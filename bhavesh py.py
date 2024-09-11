2644fc7d164cb52c567d2a56390b3d320e6e923d
bhaveshb1012 <BhaveshbhaiRameshbhai.Bhadiyadra@metropolia.fi>
2024-09-11 10:48:16 +0300

    Data Structure 10:48

--- bhavesh py.py
+++ bhavesh py.py
@@ -1,17 +1,15 @@
-list3 = []
+asymptotic_growth = {
+    'a': ['O(1)', 'O(log n)', 'O(n log n)'],
+    'b': ['O(n)', 'O(2^n)'],
+    'c': ['O(n log n)', 'O(n^2)', 'O(n^3)']
+}
 
-names = input("Enter your name: ")
-while names != "Exit":
-    list3.append(names)  # Add the name to the list
-    names = input("Enter names (or type 'Exit' to finish): ")
-print("Your list of names:", list3)
-n = input("Do you want to remove an element from the list? (Y or N): ")
-if n == 'Y' or n == 'y':
-    element_to_remove = input("Enter the name you want to remove: ")
-    if element_to_remove in list3:
-        list3.remove(element_to_remove)
-        print(f"{element_to_remove} has been removed.")
-    else:
-        print(f"{element_to_remove} is not in the list.")
+print(asymptotic_growth)
 
-print("Updated list of names:", list3)
+big_o_notations = {
+    'a': 'O(n^5)',
+    'b': 'O(n)',
+    'c': 'O(n^3)'
+}
+
+print(big_o_notations)
